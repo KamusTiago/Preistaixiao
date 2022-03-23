7aff3ba407ece7b381d6a5ccadb3796e31060ac2
Kamus Tiago <tiagokamus40@gmail.com>
2022-03-23 17:24:46 -0300

    tela home

--- android/app/src/home/Home.js
+++ android/app/src/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Button, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 
 class Home extends Component {
@@ -12,18 +12,26 @@ class Home extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.textoHome}>
+                <Image style={{width: 150, height: 150}} source={require('../../../app/assets/img/logopsbranca.png')}/>
                     <Text style={styles.estiloTextoHome}>Bem-vindo(a) ao </Text>
                     <Text style={styles.estiloTextoHome}>Preistaixion App </Text>
                 </View>
-                <View>
+                <View style={{alignItems: 'center', paddingBottom: 50}}>
                     <Text style={{
                         color: '#ffff',
-                        fontSize: 25,
+                        fontSize: 20,
                         alignItems: 'center',
-                        justifyContent: 'center'
+                        justifyContent: 'center',
+                        paddingBottom: 15
                     }}>Vamos lá!</Text>
+                <TouchableOpacity style={styles.botao}>
+                    <View style={styles.btnArea}>
+                        <Text style={styles.btnTexto}>
+                                Iniciar sessão
+                        </Text>
+                    </View>
+                </TouchableOpacity>
                 </View>
-                <Button  title='Iniciar sessão'/>
             </View>
         )
     }
@@ -46,5 +54,22 @@ const styles = StyleSheet.create({
         fontSize: 30,
         color: '#ffff',
     },
-    
+    botao:{
+        width: 300,
+        height: 50,
+        borderWidth: 2,
+        borderColor: '#ffff',
+        borderRadius: 25,
+        backgroundColor: '#ffff'
+    },
+    btnArea: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      btnTexto: {
+        fontSize: 18,
+        fontWeight: 'bold'
+      }
 })
